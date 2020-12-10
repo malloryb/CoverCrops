dd024c3318eb65551b6b3d4c09732b0acda71263
malloryb <mallorylbarnes@gmail.com>
2020-12-10 11:44:52 -0500

    Initial comit RF code

---
+++ 2_Random_Forest_Runs.R
@@ -0,0 +1,293 @@
+#"Clean" Start to random forest model runs
+library(raster)
+library(randomForest)
+library(caret)
+library(dplyr)
+library(caTools)
+#raster files
+#Now need to add ARD LST dataset:
+mean_na_x <- function(x) {
+  e <- extent(-88.09776,-84.784579,	37.771742, 41.760592)
+  r2 <- raster(e)
+  res(r2) <- c(0.00356, 0.00266)
+  crs(r2) <- "+proj=longlat +datum=WGS84 +no_defs"
+  x <- stack(x)
+  x <- x[[1:3]]
+  y <- mean(x,na.rm=T)
+  y <- extend(y, e)
+  y <- resample(y,r2)
+  print(res(y))
+  return(y)
+}
+
+#First load all the stacks 
+#LSTlist <- list.files(path="/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana", recursive = FALSE, pattern = "*.tif", full.names = TRUE)
+#Then get the mean of the oct/nov observations
+#liststack <- lapply(LSTlist, FUN=stack)
+#Then mosaic all togteher
+#LSTmerged <- do.call(merge, lapply(liststack, mean_na_x))
+#writeRaster(LSTmerged, "/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/Input_LST.tif")
+
+LSTmerged <- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/Input_LST.tif")
+plot(LSTmerged)
+print(LSTmerged)
+t16SDH<- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16SDH_input_stack.tif")
+w <- crop(LSTmerged, t16SDH)
+w <- resample(w, t16SDH)
+t16SDH <- stack(t16SDH, w)
+names(t16SDH) <- c("B3_med", "B5_med", "B6_med", "NDVI_med", "NDVI_mean", "NDVI_max", "NDVI_min", "NDVI_fullmax", "NDVI_amp", 
+                   "NDVI_ratio", "GDD", "SINDRI_med", "STI_med", "LST_unscaled")
+
+
+#16SCH<- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16SCDH_input_stack.tif")
+#16SDG <- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16SDG_input_stack.tif")
+t16TDK<- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16TDK _input_stack.tif")
+t <- crop(LSTmerged, t16TDK)
+t <- resample(t, t16TDK)
+t16TDK <- stack(t16TDK, t)
+names(t16TDK) <- c("B3_med", "B5_med", "B6_med", "NDVI_med", "NDVI_mean", "NDVI_max", "NDVI_min", "NDVI_fullmax", "NDVI_amp", 
+                   "NDVI_ratio", "GDD", "SINDRI_med", "STI_med", "LST_unscaled")
+
+t16TDL<- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16TDL _input_stack.tif")
+y <- crop(LSTmerged, t16TDL)
+y <- resample(y, t16TDL)
+t16TDL <- stack(t16TDL, y)
+names(t16TDL) <- c("B3_med", "B5_med", "B6_med", "NDVI_med", "NDVI_mean", "NDVI_max", "NDVI_min", "NDVI_fullmax", "NDVI_amp", 
+                   "NDVI_ratio", "GDD", "SINDRI_med", "STI_med", "LST_unscaled")
+
+t16TEL<- stack("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/16TEL_input_stack.tif")
+z <- crop(LSTmerged, t16TEL)
+z <- resample(z, t16TEL)
+t16TEL <- stack(t16TEL, z)
+names(t16TEL) <- c("B3_med", "B5_med", "B6_med", "NDVI_med", "NDVI_mean", "NDVI_max", "NDVI_min", "NDVI_fullmax", "NDVI_amp", 
+                   "NDVI_ratio", "GDD", "SINDRI_med", "STI_med", "LST_unscaled")
+
+#input data csv------
+gc()
+#All_counties_input <- read.csv("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/RS_input_all.csv")
+All_counties_input <- read.csv("/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/inputs_with_LST_unscaled.csv")
+All_counties_input$Cover_Crop <- as.factor(All_counties_input$Cover_Crop)
+levels(All_counties_input$Cover_Crop)
+plyr::count(All_counties_input$Cover_Crop)
+
+levels(as.factor(All_counties_input$Fall_Tilla))
+plyr::count(All_counties_input$Fall_Tilla)
+
+levels(as.factor(All_counties_input$CC_Method))
+plyr::count(All_counties_input$CC_Method)
+
+#Two class model-------
+#Cover Crop Presence Absence Category: 0 = no cover crop,  2 = "Cover Crop" 
+#Cover Crop Specific Category: 0 = nothing (presumably conventional tillage), 1= 'No-Till', 2 = "Cover crop".  
+All_counties_input$Cover_Crop_Specific <- "0"
+All_counties_input$Cover_Crop_Specific[All_counties_input$Fall_Tilla == "N" | All_counties_input$Fall_Tilla == "n"] <- "1"
+All_counties_input$Cover_Crop_Specific[All_counties_input$Cover_Crop != "N"] <- "2"
+All_counties_input$Cover_Crop_Specific <- as.factor(All_counties_input$Cover_Crop_Specific)
+levels(as.factor(All_counties_input$Cover_Crop_Specific))
+
+#recode cover crop: 
+All_counties_input$Cover_Crop_PA <- "0"
+All_counties_input$Cover_Crop_PA[All_counties_input$Cover_Crop == "A" | All_counties_input$Cover_Crop=="B" | All_counties_input$Cover_Crop=="BC"| All_counties_input$Cover_Crop=="C"| All_counties_input$Cover_Crop=="W" | All_counties_input$Cover_Crop=="G"] <- "2"
+All_counties_input$Cover_Crop_PA <- as.factor(All_counties_input$Cover_Crop_PA)
+head(All_counties_input)
+All_counties_input <- subset(All_counties_input, select=-c(s023010_fall))
+plyr::count(All_counties_input$Cover_Crop_PA)
+plyr::count(All_counties_input$Cover_Crop_Specific)
+All_counties_input <- na.omit(All_counties_input)
+#random forest model develop
+#Gonna try github method from here: https://gist.github.com/hakimabdi/720f1481af9eca0b7b97d9856052e0e2
+# Split the data frame into 70-30 by class
+#First grouping------
+levels(All_counties_input$Cover_Crop_PA)
+sample <-sample.split(All_counties_input$Cover_Crop_PA, SplitRatio = 0.8)
+trn=subset(All_counties_input, sample==TRUE)
+eva=subset(All_counties_input, sample==FALSE)
+eva.sp = SpatialPointsDataFrame(coords = cbind(eva$R_Lon, eva$R_Lat), data = eva, proj4string = crs("+proj=longlat +datum=WGS84 +no_defs"))
+# Set up a resampling method in the model training process
+tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
+                   number = 10, # number of folds
+                   repeats = 5, # number of repeats
+                   allowParallel = FALSE, # allow use of multiple cores if specified in training
+                   verboseIter = TRUE) # view the training iterations
+
+# Generate a grid search of candidate hyper-parameter values for inclusion into the models training
+# These hyper-parameter values are examples. You will need a more complex tuning process to achieve high accuracies
+# For example, you can play around with the parameters to see which combinations gives you the highest accuracy. 
+rf.grid <- expand.grid(mtry=1:10) # number of variables available for splitting at each tree node
+
+## Begin training the models. It took my laptop 8 minutes to train all three algorithms
+# Train the random forest model
+head(trn)
+rf_modelLST <- caret::train(x = trn[,(9:22)], y = as.factor(trn$Cover_Crop_PA),
+                         method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelSWIR <- caret::train(x = trn[,(9:21)], y = as.factor(trn$Cover_Crop_PA),
+                         method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelVISNir <- caret::train(x = trn[,(9:19)], y = as.factor(trn$Cover_Crop_PA),
+                         method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelNDVI <- caret::train(x = trn[12], y = as.factor(trn$Cover_Crop_PA),
+                         method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+
+rf_modelLST
+rf_modelSWIR
+rf_modelVISNir
+rf_modelNDVI
+
+rf_model
+varImp(rf_modelNDVI)
+varImp(rf_modelVISNir)
+varImp(rf_modelSWIR)
+varImp(rf_modelLST)
+
+varImpPlot(rf_model)
+
+save(rf_model,file = "/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/RandomForest_LST_PA.RData")
+#random forest model evalaute
+## Apply the models to data. It took my imac 16 minutes to apply the random forest model
+# Apply the random forest model to the HLS data-----
+gc()
+rf_prediction1 = raster::predict(t16SDH, model=rf_model)
+rf_prediction2= raster::predict(t16TDK, model=rf_model)
+rf_prediction3 = raster::predict(t16TDL, model=rf_model)
+rf_prediction4 = raster::predict(t16TEL, model=rf_model)
+
+plot(rf_prediction1)
+plot(rf_prediction2)
+plot(rf_prediction3)
+plot(rf_prediction4)
+
+#rf_prediction_prob = raster::predict(t16SDH, model=rf_model, type="prob")
+
+# Convert the evaluation data into a spatial object using the X and Y coordinates and extract predicted values
+# Create an error matrix for each of the classifiers
+rf_Eval1 = extract(rf_prediction1, eva.sp)
+rf_Eval2 = extract(rf_prediction2, eva.sp)
+rf_Eval3 = extract(rf_prediction3, eva.sp)
+rf_Eval4 = extract(rf_prediction4, eva.sp)
+e <- extent(-88.09776,-84.784579,	37.771742, 41.760592)
+r2 <- raster(e)
+res(r2) <- c(0.00356, 0.00266)
+crs(r2) <- "+proj=longlat +datum=WGS84 +no_defs"
+f <- extend(rf_prediction1, e)
+f <- resample(f, r2)
+d <- extend(rf_prediction2, e)
+d <- resample(d,r2)
+c <- extend(rf_prediction3, e)
+c <- resample(c,r2)
+b <- extend(rf_prediction4, e)
+b <- resample(b,r2)
+plot(calc(stack(f,d,c,b), mean_na))
+
+length(rf_Eval3)
+length(eva$Cover_Crop_PA)
+rf_errorM1 = confusionMatrix(as.factor(rf_Eval1),as.factor(eva$Cover_Crop_PA))
+rf_errorM2 = confusionMatrix(as.factor(rf_Eval2),as.factor(eva$Cover_Crop_PA))
+rf_errorM3 = confusionMatrix(as.factor(rf_Eval3),as.factor(eva$Cover_Crop_PA))
+rf_errorM4 = confusionMatrix(as.factor(rf_Eval4),as.factor(eva$Cover_Crop_PA))
+rf_errorM1
+rf_errorM2
+rf_errorM3
+rf_errorM4
+
+gc()
+#Second grouping- 3 class model-----
+levels(All_counties_input$Cover_Crop_Specific)
+sample <-sample.split(All_counties_input$Cover_Crop_Specific, SplitRatio = 0.70)
+trn=subset(All_counties_input, sample==TRUE)
+eva=subset(All_counties_input, sample==FALSE)
+levels(All_counties_input$Cover_Crop_Specific)
+# Set up a resampling method in the model training process
+tc <- trainControl(method = "repeatedcv", # repeated cross-validation of the training data
+                   number = 10, # number of folds
+                   repeats = 5, # number of repeats
+                   allowParallel = FALSE, # allow use of multiple cores if specified in training
+                   verboseIter = TRUE) # view the training iterations
+
+# Generate a grid search of candidate hyper-parameter values for inclusion into the models training
+# These hyper-parameter values are examples. You will need a more complex tuning process to achieve high accuracies
+# For example, you can play around with the parameters to see which combinations gives you the highest accuracy. 
+rf.grid <- expand.grid(mtry=1:10) # number of variables available for splitting at each tree node
+
+## Begin training the models. It took my laptop 8 minutes to train all three algorithms
+# Train the random forest model
+rf_model2 <- caret::train(x = trn[,(9:22)], y = as.factor(trn$Cover_Crop_Specific),
+                         method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+
+rf_modelLST <- caret::train(x = trn[,(9:22)], y = as.factor(trn$Cover_Crop_Specific),
+                            method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelSWIR <- caret::train(x = trn[,(9:21)], y = as.factor(trn$Cover_Crop_Specific),
+                             method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelVISNir <- caret::train(x = trn[,(9:19)], y = as.factor(trn$Cover_Crop_Specific),
+                               method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+rf_modelNDVI <- caret::train(x = trn[12], y = as.factor(trn$Cover_Crop_Specific),
+                             method = "rf", metric="Kappa", trainControl = tc, tuneGrid = rf.grid)
+
+rf_modelLST
+rf_modelSWIR
+rf_modelNDVI
+rf_modelVISNir
+
+
+rf_model2
+varImp(rf_model2)
+save(rf_model2,file = "/Volumes/G-RAID_Thunderbolt3/HLS30_Indiana/2015_2016_Input_Bands/RandomForest_LST_Cat.RData")
+
+#random forest model evalaute
+## Apply the models to data. It took my imac 16 minutes to apply the random forest model
+# Apply the random forest model to the HLS data
+rf2_prediction1 = raster::predict(t16SDH, model=rf_model2)
+rf2_prediction2= raster::predict(t16TDK, model=rf_model2)
+rf2_prediction3 = raster::predict(t16TDL, model=rf_model2)
+rf2_prediction4 = raster::predict(t16TEL, model=rf_model2)
+
+e <- extent(-88.09776,-84.784579,	37.771742, 41.760592)
+r2 <- raster(e)
+res(r2) <- c(0.00356, 0.00266)
+crs(r2) <- "+proj=longlat +datum=WGS84 +no_defs"
+x <- extend(rf2_prediction1, e)
+x <- resample(x, r2)
+y <- extend(rf2_prediction2, e)
+y <- resample(y,r2)
+z <- extend(rf2_prediction3, e)
+z <- resample(z,r2)
+a <- extend(rf2_prediction4, e)
+a <- resample(a,r2)
+
+tst <- stack(x,y,z,a)
+plot(calc(tst,mean_na))
+
+#rf_prediction_prob = raster::predict(t16SDH, model=rf_model, type="prob")
+
+# Convert the evaluation data into a spatial object using the X and Y coordinates and extract predicted values
+# Create an error matrix for each of the classifiers
+length(eva$Cover_Crop_Specific)
+eva.sp = SpatialPointsDataFrame(coords = cbind(eva$R_Lon, eva$R_Lat), data = eva, proj4string = crs("+proj=longlat +datum=WGS84 +no_defs"))
+length(eva.sp)
+rf2_Eval1 = extract(rf2_prediction1, eva.sp)
+rf2_Eval2 = extract(rf2_prediction2, eva.sp)
+rf2_Eval3 = extract(rf2_prediction3, eva.sp)
+rf2_Eval4 = extract(rf2_prediction4, eva.sp)
+rf2_errorM1 = confusionMatrix(as.factor(rf2_Eval1),as.factor(eva$Cover_Crop_Specific))
+rf2_errorM2 = confusionMatrix(as.factor(rf2_Eval2),as.factor(eva$Cover_Crop_Specific))
+rf2_errorM3 = confusionMatrix(as.factor(rf2_Eval3),as.factor(eva$Cover_Crop_Specific))
+rf2_errorM4 = confusionMatrix(as.factor(rf2_Eval4),as.factor(eva$Cover_Crop_Specific))
+rf2_errorM1
+rf2_errorM2
+rf2_errorM3
+rf2_errorM4
+#rf_prediction_prob = raster::predict(t16SDH, model=rf_model, type="prob")
+
+
+# Convert the evaluation data into a spatial object using the X and Y coordinates and extract predicted values
+plot(rf_prediction2)
+writeRaster(rf_prediction2,"/Volumes/Macintosh HD/Users/malbarn/Documents/CoverCrop_Project/rf_prediction_posey_cat2.tif", overwrite=TRUE) 
+#writeRaster(rf_prediction_prob, "/Volumes/Macintosh HD/Users/malbarn/Documents/CoverCrop_Project/rf_prediction_prob_posey_cat2.tif")
+plot(rf_prediction_prob)
+varImp(rf_model2)
+## Superimpose evaluation points on the predicted classification and extract the values
+# random forest
+rf_Eval = extract(rf_prediction2, eva.sp)
+
+# Create an error matrix for each of the classifiers
+rf_errorM2 = confusionMatrix(as.factor(rf_Eval),as.factor(eva$Cover_Crop_Specific))
+rf_errorM2
+#plots
